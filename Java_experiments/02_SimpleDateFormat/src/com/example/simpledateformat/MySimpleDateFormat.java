996ba8e38cb33051353c0be7e1d8ca2cb8eb6a00
Maksym Riaboshapka <makmr11@gmail.com>
2019-01-22 18:56:55 +0200

    02_added_SimpleDateFormat_folder

--- Java_experiments/02_SimpleDateFormat/src/com/example/simpledateformat/MySimpleDateFormat.java
+++ Java_experiments/02_SimpleDateFormat/src/com/example/simpledateformat/MySimpleDateFormat.java
@@ -1,21 +1,30 @@
 package com.example.simpledateformat;
 
 /*
-* Get a new data format from the old one.
-*/
+ * Get a new data format from the old one.
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class SimpleDateFormat {
+public class MySimpleDateFormat {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // input in console "2012-06-15"
         String inputDateFormat = bufferedReader.readLine();
 
-        SimpleDateFormat oldDateFormat = new SimpleDateFormat("yyyy-dd-MM");
+        SimpleDateFormat oldDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat newDateFormat = new SimpleDateFormat("MMM dd, yyyy");
+
+        Date date = oldDateFormat.parse(inputDateFormat);
+
+        System.out.println(newDateFormat.format(date).toUpperCase());
 
     }
 
